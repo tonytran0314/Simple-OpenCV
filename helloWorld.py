fa67a4257b01e5fd7b8ce418a6ff932b395d6708
Huy Tran <55000985+tonytran0314@users.noreply.github.com>
2021-08-18 01:24:11 -0400

    Add notes

--- helloWorld.py
+++ helloWorld.py
@@ -1,10 +1,16 @@
+# Open an image
+
+# import cv2 library
 import cv2
 
+# read giadinh.jpg image and assign to img variable
 img = cv2.imread("giadinh.jpg")
 
-px = img[0][0]
+# Open the image
+# giadinh.jpg will be displayed as file title | img as a reference
+cv2.imshow("giadinh.jpg", img) 
 
-print(px)
-
+# Wait until I turn it off
 cv2.waitKey(0)
+#destroy all windows when I turn the image off
 cv2.destroyAllWindows()
